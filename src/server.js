0817e6c23ac603bf00605423bf2066ac7c726de2
Paulo Melo <peimelo@gmail.com>
2020-04-14 13:48:20 -0300

    Config to Heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const port = process.env.NODE_ENV === 'production' ? 8080 : 3333;
+const port = process.env.NODE_ENV === 'production' ? 80 : 3333;
 
 app.listen(port);
