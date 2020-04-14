f39ea7b22fe24db4d88b2a99cec68c37b1febc52
Paulo Melo <peimelo@gmail.com>
2020-04-14 14:46:07 -0300

    Config to Heroku

--- src/app.js
+++ src/app.js
@@ -14,6 +14,34 @@ app.get('/', (req, res) => {
   const help = `
   <pre>
     Welcome to Repositories!
+
+    GET /repositories
+      USAGE:
+        Get all repositories.
+
+    POST /repositories
+      USAGE:
+        Add a new repository.
+      BODY:
+        title - String
+        url - String
+        techs - String[]
+
+    PUT /repositories/:id
+      USAGE:
+        Edit the details of an existing repository.
+      BODY:
+        title - String
+        url - String
+        techs - String[]
+
+    DELETE /repositories/:id
+      USAGE:
+        Delete an existing repository.
+
+    POST /repositories/:id/like
+      USAGE:
+        Increment repository like.
   </pre>
   `;
 
