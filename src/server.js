f9006cbc7e1ea9356e341ea6d820e76c5537903e
Paulo Melo <peimelo@gmail.com>
2020-04-14 14:35:04 -0300

    Config to Heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const port = process.env.NODE_ENV === 'production' ? 8080 : 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => console.log('Express server listening on port', port));
