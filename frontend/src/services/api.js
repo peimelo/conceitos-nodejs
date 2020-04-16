21d6b2d22fe0cf606aa7fd0492e3d94987b1a044
Paulo Melo <peimelo@gmail.com>
2020-04-16 07:09:26 -0300

    baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3333';
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:3333',
+  baseURL,
 });
 
 export default api;
